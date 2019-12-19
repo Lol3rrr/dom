13cdd853a52a8799e6ea9e108eb6d4c82c9b6cd2
lol3rrr <s.loler03@gmail.com>
2019-12-19 18:03:24 +0100

    Changed the parseElementPart function a bit

--- parseElementPart.go
+++ parseElementPart.go
@@ -4,10 +4,11 @@ import (
   "strings"
 )
 
+const divider string = "=\""
+const dividerLength int = 2
+
 // parseElementPart parses one part of an elements Attributes
 func parseElementPart(input string) (string, string) {
-  divider := "=\""
-  dividerLength := 2
   dividerIndex := strings.Index(input, divider)
   if dividerIndex == -1 || dividerIndex == 0 {
     return "", ""
